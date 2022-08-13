127677ad848458bc6b25aa28a724f20458a58358
Mira Ehab <miraeha2@gmail.com>
2022-08-13 21:19:49 +0200

    Solved Problem of Day 7 (1220. Count Vowels Permutation)

---
+++ 1220. Count Vowels Permutation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    vector<vector<int>> relation={{1,2,4},{0,2},{1,3},{2},{2,3}};
+public:
+    int countVowelPermutation(int n) {
+        int MOD = 1e9+7;
+        vector<long long> vowels(5,1),vowels_copy;      
+        long long ans = 0;
+        while(--n){
+            vowels_copy = vowels;
+            for(int i = 0; i < 5; i++){
+                vowels[i] = 0;
+                for(int &r:relation[i]){
+                    vowels[i] += vowels_copy[r];
+                    vowels[i] %= MOD; 
+                } 
+            }
+        }
+        
+        for(long long &i:vowels){
+            ans += i;
+            ans %= MOD;
+        }
+        return ans;
+    }
+};
