f12cd1ad72eedfdf20bd9f830fa29fc5caba05ab
Mira Ehab <miraeha2@gmail.com>
2022-08-25 15:00:52 +0200

    Solved Problem of Day 22 (342. Power of Four)

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    float logn(int n, int r){
+        return log(n) / log(r);
+    }
+    
+    bool isPowerOfFour(int n) {
+        if(n == 0)
+            return false;
+        return (floor(logn(n,4)) == ceil(logn(n,4)));
+    }
+};
